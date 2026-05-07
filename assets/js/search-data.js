688a1e0e2f5a21044ede3878be87121711562bfc
lanhao34 <lanhao34@gmail.com>
2026-05-07 20:03:42 +0000

    Deploying to gh-pages from @ lanhao34/lanhao34.github.io@95284519c66f39055477ad4996f899a9272c25ad 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,7 +23,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{
+        },{id: "news-a-simple-inline-announcement",
+          title: 'A simple inline announcement.',
+          description: "",
+          section: "News",},{id: "news-a-long-announcement-with-details",
+          title: 'A long announcement with details',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_2/";
+            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
